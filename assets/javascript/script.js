c4d2ed5b1fad41549c31f879a3e842707436a2b3
i-k0n <mbaker.github@gmail.com>
2019-06-07 18:52:16 -0500

    got automatic button text color working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,33 +12,48 @@ function addButtons() {
         // Adding a data-attribute
         newButton.attr("data-name", color);
         // Providing the initial button text
-        newButton.text(color);
+        newButton.text(color.toUpperCase());
         newButton.css({'background-color': color});
         // Adding the button to the buttons-view div
         colorButtonDiv.append(newButton);
 
         // use colors.js to split its RGB output into an array for checking values
         var splitColor = $c.name2rgb(color).RGB.split(" ");
+        // set up variables for the passed arguments
         var r = splitColor[0];
         var g = splitColor[1];
         var b = splitColor[2];
-
-        chooseLabelColor(r, g, b);
-        if (!chooseLabelColor) {
-            newButton.css('color', 'black');
-        } else {
-            newButton.css('color', 'white');
+        var chooseLabelColor = function(r, g, b) {
+            // W3C formula for checking contrast, returns value between 0 - 255:
+            // ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
+            brightnessValue = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+            // console.log("brightnessValue: ", brightnessValue);
+            if (brightnessValue > 127) {
+                // bright, use black text
+                newButton.css('color', 'black');
+                // return true;
+            } else {
+                // dark, use white text
+                newButton.css('color', 'white');
+                // return false;
+            }
         }
-        
-        // console.log(splitColor)
+
+        // run function to check background brightness for choosing the correct text color
+        chooseLabelColor(r, g, b);
+
+        // use returned value to set text color
+        // if (!chooseLabelColor) {
+        //     newButton.css('color', 'black');
+        // } else {
+        //     newButton.css('color', 'white');
+        // }
     });
 }
 
 var chooseLabelColor = function(r, g, b) {
-    // W3C formula for checking contrast
+    // W3C formula for checking contrast, returns value between 0 - 255:
     // ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
-    // if our value is > 50% use black text
-    // else use white text
     brightnessValue = ((r * 299) + (g * 587) + (b * 114)) / 1000;
     // console.log("brightnessValue: ", brightnessValue);
     if (brightnessValue > 127) {
@@ -48,7 +63,6 @@ var chooseLabelColor = function(r, g, b) {
         // dark, use white text
         return false;
     }
-
 }
 
 // keep that page from reloading on click
